529c75ae6381b3964f14ad7b112786c4269e7efb
damianbrudkowski <damheros@gmail.com>
2025-10-11 20:38:22 +0200

    Initial Commit

---
+++ ChineseFooDranking/ChineseFooDrankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseFooDrankingApp.swift
+//  ChineseFooDranking
+//
+//  Created by Damian Brudkowski on 11/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChineseFooDrankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

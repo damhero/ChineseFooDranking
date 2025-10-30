747e8e08b7cb1615807afe4b975a30d886dd7ebc
damianbrudkowski <damheros@gmail.com>
2025-10-30 12:17:00 +0100

    Main header and champions functionality

---
+++ ChineseFooDranking/Components/Champions.swift
@@ -0,0 +1,18 @@
+//
+//  Champions.swift
+//  ChineseFooDranking
+//
+//  Created by Damian Brudkowski on 30/10/2025.
+//
+
+import SwiftUI
+
+struct Champions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Champions()
+}
